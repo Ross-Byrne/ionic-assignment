f328afb4b26fb17694bc115cdfd5edc51f6b2182
Ross Byrne <ross.byrne@outlook.ie>
2015-04-26 16:55:16 +0100

    Getting ready
    In this branch I will work on the Settings menu.

--- www/js/app.js
+++ www/js/app.js
@@ -1,46 +1,46 @@
 angular.module('Calorie Counter', ['ionic', 'Calorie Counter.controllers', 'Calorie Counter.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+      		cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+	$stateProvider
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html"
-  })
+	.state('app', {
+		url: "/app",
+    	abstract: true,
+    	templateUrl: "templates/menu.html"
+	})
 
-  .state('app.home', {
-    url: "/home",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/home.html",
-		controller: 'HomeCtrl'
-      }
-    }
-  })
+	.state('app.home', {
+		url: "/home",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/home.html",
+				controller: 'HomeCtrl'
+			}
+		}
+	})
 
   .state('app.settings', {
     url: "/settings",
     views: {
-      'menuContent': {
-        templateUrl: "templates/settings.html"
+			'menuContent': {
+				templateUrl: "templates/settings.html"
       }
     }
   });
 	
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
+		// if none of the above states are matched, use this as the fallback
+		$urlRouterProvider.otherwise('/app/home');
 });
